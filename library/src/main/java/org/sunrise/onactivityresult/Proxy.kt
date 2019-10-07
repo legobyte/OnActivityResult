7ec636b809fbf7e0e0b43c9af3be669931843580
Ramin <raminboodaghi@gmail.com>
2019-10-08 01:28:34 +0330

    first commit

---
+++ library/src/main/java/org/sunrise/onactivityresult/Proxy.kt
@@ -0,0 +1,52 @@
+package org.sunrise.onactivityresult
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.ResultReceiver
+import androidx.annotation.CheckResult
+
+
+object Proxy {
+    @JvmStatic
+    val mainThreadHandler by lazy { Handler(Looper.getMainLooper()) }
+    const val KEY_INTENT = "sunrise:intent"
+    const val KEY_REQUEST_CODE = "sunrise:request-code"
+    const val KEY_RESULT_RECEIVER = "sunrise:result-receiver"
+    const val DEFAULT_REQUEST_CODE = 10
+    const val RESULT_INTENT_UNHANDLED = -10
+
+    fun with(context: Context): ContextBucket {
+        return ContextBucket(context)
+    }
+}
+
+data class ContextBucket(internal val context: Context) {
+    @CheckResult
+    fun listener(resultReceiver: (requestCode: Int, resultCode: Int, data: Intent?) -> Unit)
+            = Launcher(this, resultReceiver)
+}
+data class Launcher(private val contextBucket: ContextBucket, private val resultReceiver: (requestCode: Int, resultCode: Int, data: Intent?) -> Unit){
+    fun launch(intent: Intent, requestCode: Int= Proxy.DEFAULT_REQUEST_CODE){
+        val receiver = object: ResultReceiver(Proxy.mainThreadHandler){
+            override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+                val resultIntent = resultData?.getParcelable<Intent>(Proxy.KEY_INTENT)
+                resultReceiver(requestCode, resultCode, resultIntent)
+            }
+        }
+        val context = contextBucket.context
+        context.startActivity(Intent(context, ProxyActivity::class.java).apply {
+            if(context is Application || context !is Activity){
+                // we can check for context wrapper too
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            putExtra(Proxy.KEY_INTENT, intent)
+            putExtra(Proxy.KEY_REQUEST_CODE, requestCode)
+            putExtra(Proxy.KEY_RESULT_RECEIVER, receiver)
+        })
+    }
+}
---
+++ library/src/main/java/org/sunrise/onactivityresult/ProxyActivity.kt
@@ -0,0 +1,46 @@
+package org.sunrise.onactivityresult
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.ResultReceiver
+
+internal class ProxyActivity : Activity() {
+
+
+    private fun exception(initialMessage:String):Throwable{
+        return IllegalArgumentException("$initialMessage You should start this activity with org.sunrise.onactivityresult.Proxy#with method!")
+    }
+
+    private val clientIntent by lazy {
+        intent.getParcelableExtra<Intent>(Proxy.KEY_INTENT) ?: throw exception("Intent cannot be null.")
+    }
+    private val resultReceiver by lazy {
+        intent.getParcelableExtra<ResultReceiver>(Proxy.KEY_RESULT_RECEIVER) ?: throw exception("ResultReceiver cannot be null.")
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Set a Translucent NoTitleBar theme before calling super.onCreate()
+        setTheme(android.R.style.Theme_Translucent_NoTitleBar)
+        super.onCreate(savedInstanceState)
+
+        if(clientIntent.resolveActivity(packageManager) == null){
+            resultReceiver.send(Proxy.RESULT_INTENT_UNHANDLED, null)
+            finish()
+        }else{
+            startActivityForResult(clientIntent, intent.getIntExtra(Proxy.KEY_REQUEST_CODE, Proxy.DEFAULT_REQUEST_CODE))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        resultReceiver.send(resultCode, Bundle().apply {
+            if(data!=null)
+                putParcelable(Proxy.KEY_INTENT, data)
+        })
+        finish()
+    }
+
+
+}
